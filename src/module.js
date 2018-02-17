988481a023d5b1f7df8f235f62ebaca6149e8328
harryho <harry.ho_long@yahoo.com>
2018-02-17 17:04:33 +1100

    Update module.js

--- src/module.js
+++ src/module.js
@@ -1,6 +1,12 @@
-function main ( param1, param2, params ) {   
+function main ( param1, param2, param3, params ) {   
    console.log( " hello ", params );
+   console.log( " hello ", param3 );
    console.log( " hello ", arguments );
+   if (params!==undefined && typeof params === "object" && params.length > 0  ){
+       params.forEach(function(e, idx) {
+            console.log( " index: ", idx, " item : ", e);
+       });
+   }
 }
 
 module.exports = main
