6a6fe9d7117c16b8d09b6176e1d19a5e6d26294c
harryho <harry.ho_long@yahoo.com>
2018-02-17 17:09:40 +1100

    Update branch-1 by jl

--- src/module.js
+++ src/module.js
@@ -1,6 +1,9 @@
-function main ( param1, param2, params ) {   
+function main ( param1, param2, params ) {  
+   console.log( " hello ", param1 ); 
    console.log( " hello ", params );
    console.log( " hello ", arguments );
+   console.log( " hello ", typeof(arguments) );
+
 }
 
 module.exports = main
